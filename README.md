9d80388b9408b8aa0b279aa32320c9dbaf239f20
Andrey Bayandin <dontusecascade@gmail.com>
2014-10-05 16:57:41 +0600

    go github

---
+++ README.md
@@ -0,0 +1,36 @@
+LoveLess
+========
+<div>
+    <a target="_blank" href="http://gruntjs.com/">Grunt</a>
+</div>
+<div>
+    <h3>Установка:</h3>
+
+    <p>
+        cd html/build
+    </p>
+    <p>
+        npm install
+    </p>
+</div>
+
+<div>
+    <h3>Сборка:</h3>
+
+    <p><b>dev:</b></p>
+    <p>
+        grunt
+    </p>
+    <p><b>prod:</b></p>
+    <p>
+        grunt prod
+    </p>
+    <p><b>watch:</b></p>
+    <p>
+        grunt watch
+    </p>
+    <p><b>Сервер с вотчингом изменений</b></p>
+        <p>
+            grunt server
+        </p>
+</div>
---
+++ static/build/Gruntfile.js
@@ -0,0 +1,126 @@
+
+
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+
+        concat: {
+            dist: {
+                src:  grunt.file.readJSON('js.json'),
+                dest: '../publish/script.js'
+            }
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    '../publish/script.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+        less: {
+            dev: {
+                options: {
+                },
+                files: {
+                    "../publish/style.css": "../blocks/love.less"
+                }
+            },
+            production: {
+                options: {
+                    yuicompress: true
+                },
+                files: {
+                    "../publish/style.css": "../blocks/love.less"
+                }
+            }
+        },
+        watch: {
+            options: {
+                atBegin: true,
+                livereload: true
+            },
+            scripts: {
+                files: ['<%= concat.dist.src %>','js.js'],
+                tasks: 'concat'
+            },
+            css: {
+                files: [
+                    '../blocks/**/*.less',
+                    '../blocks/*.less'
+                ],
+                tasks: 'less:dev'
+
+            },
+            pages : {
+                files: ['../pages/**/*.hbs' , '../parts/**/*.hbs', '../layouts/**/*.hbs' ],
+                tasks: 'assemble'
+            }
+        },
+        imageEmbed: {
+            dist: {
+                src: ['../publish/style.css'],
+                dest: '../publish/style.css',
+                options: {
+                    deleteAfterEncoding : false
+                }
+            }
+        },
+        assemble: {
+            options: {
+                data: ['../data/*.json'],
+                layoutdir: '../layouts',
+                layout: 'main.hbs',
+                tmpPath : 'html/tmp/',
+                blockPath : 'html/block',
+                stylePath : 'html/publish',
+                jsPath : 'html/js',
+                partials: ['../parts/**/*.hbs' , '../blocks/**/*.hbs' ],
+               // data: ['../data/*.{json,yml}'],
+                flatten: true
+            },
+            pages: {
+                src: ['../pages/*.hbs'],
+                dest: '../..'
+            }
+        },
+        // internal server
+        connect: {
+            server: {
+                options: {
+                    livereload: true,
+                    base: '../../'
+                }
+            }
+        },
+        copy: {
+            main: {
+                src: '../js/',
+                dest: 'create-me'
+            }
+        }
+    });
+
+
+    grunt.loadNpmTasks('grunt-contrib-concat');    // конкатит файлы
+    grunt.loadNpmTasks('grunt-contrib-less');     // компилит less / жмет
+    grunt.loadNpmTasks('grunt-contrib-uglify');  // жмет js
+    grunt.loadNpmTasks('grunt-contrib-watch');  // вотчер
+    //grunt.loadNpmTasks("grunt-image-embed");   // конвертит картинке в base64
+    grunt.loadNpmTasks('assemble');           // сборка html
+    grunt.loadNpmTasks('grunt-contrib-connect');  // сервер
+
+
+    // main dev task
+    grunt.registerTask('default', [ 'concat' , 'less:dev' , 'assemble']);
+
+    // server
+    grunt.registerTask('server', ['connect', 'watch']);
+
+    // production
+    grunt.registerTask('prod', ['concat', 'uglify' , 'less:production' , 'imageEmbed'] );
+
+
+
+
+};
